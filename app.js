b3542abc275a1ddfe4a6e3084ad0b6ff9d42600d
akash20042 <akashprinsh728@gmail.com>
2024-03-05 16:23:21 +0530

    this is for the test

---
+++ app.js
@@ -0,0 +1,7 @@
+ 
+
+ const heading = React.createElement("h1" , {id:"parent"}, " this  only for the test")
+ const Root =  ReactDOM.createRoot(document.getElementById("root"))
+  Root.render(heading)
+
+ 
